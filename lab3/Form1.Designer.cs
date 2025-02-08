c5574a2efd320c2b9e7761f51dec6f8c5dafe203
TimRobin10 <timrobinremeticado@gmail.com>
2025-02-08 21:51:52 +0800

    Lab 3 commit

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,51 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            showInfoButton = new Button();
+            bookListBox = new ListBox();
+            SuspendLayout();
+            // 
+            // showInfoButton
+            // 
+            showInfoButton.Location = new Point(44, 171);
+            showInfoButton.Name = "showInfoButton";
+            showInfoButton.Size = new Size(257, 65);
+            showInfoButton.TabIndex = 0;
+            showInfoButton.Text = "Show Book Info";
+            showInfoButton.UseVisualStyleBackColor = true;
+            showInfoButton.Click += showInfoButton_Click;
+            // 
+            // bookListBox
+            // 
+            bookListBox.FormattingEnabled = true;
+            bookListBox.Location = new Point(335, 28);
+            bookListBox.Name = "bookListBox";
+            bookListBox.Size = new Size(431, 384);
+            bookListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(bookListBox);
+            Controls.Add(showInfoButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button showInfoButton;
+        private ListBox bookListBox;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,91 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string Title { get; set; }
+
+            public string Author { get; set; }
+
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+
+        public class Magazine : Book
+        {
+            public int issueNumber { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} - Issue {issueNumber}";
+            }
+        }
+
+        public class textBook : Book
+        {
+            public string subject { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} - Subject: {subject}";
+            }
+        }
+
+        public class audioBook : Book
+        {
+            public int duration { get; set; }
+            public string narrator { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} - Narrated by {narrator} - Duration: {duration} minutes";
+            }
+        }
+
+        public void loadBooks(List<Book> book, ListBox listbox)
+        {
+            listbox.Items.Clear();
+            foreach(var books in book)
+            {
+                listbox.Items.Add(books.GetInfo());
+            }
+        }
+        private void showInfoButton_Click(object sender, EventArgs e)
+        {
+            List<Book> books = new List<Book>
+            {
+                new Book
+                {
+                    Title = "Harry Potter",
+                    Author = "J.K Rowling"
+                },
+                new Magazine
+                {
+                    Title = "Time Magazine",
+                    issueNumber = 234,
+                },
+                new textBook
+                {
+                    Title = "Life and Works of Rizal",
+                    Author = "Rey Atacador"
+                },
+                new audioBook
+                {
+                    Title = "The Imitation Game",
+                    narrator = "Alan Turing",
+                    duration = 10
+                }
+            };
+
+            loadBooks(books, bookListBox);
+        }
+    }
 }
